2189aa794b0f565fe93b30d786cfb10bdb7efcb4
Frost <frostedwolf789@gmail.com>
2022-11-03 16:58:05 -0400

    Add initialState

--- sudoku.cpp
+++ sudoku.cpp
@@ -3,28 +3,43 @@ using namespace std;
 
 class sudoku {
     public:
-        int domain[9*9];
+        int variables[9][9];
+        int domain[81][8]; //9*9
+        int row[9][9];
+        int column[9][9];
+        int box[9][9];
+        int arcC[9][9];
 
-    sudoku(int* board) {
-        int c = 1;
+    sudoku() {
+
+    }
+
+    void initialState(){
+        int count=0;
+        int val=0;
+        ifstream file;
+        string fileName;
+        cout << "Please input the puzzle text file name: " << endl;
+        cin >> fileName;
+        file.open(fileName);
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                file >> val;
+                if (val != 0){
+                    variables[i][j] = val;
+                    row[i][j]=val;
+                    column[j][count++]=val;
+                }
+            }
+        }
+        file.close();
+        return;
     }
 
 };
 
 int main(int argc, char const *argv[]) {
-    ifstream file;
-    string fileName;
-    int grid[9][9];
-    cout << "Please input the puzzle text file name: " << endl;
-    cin >> fileName;
-    file.open(fileName);
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            file >> grid[i][j];
-        }
-    }
-    file.close();
-    cout << "Complete." << endl;
+    sudoku s;
     
     return 0;
 }
