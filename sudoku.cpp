ebd9f589935f449bca86a054e8bbbf5d8f41341c
Frost <frostedwolf789@gmail.com>
2022-11-03 18:47:05 -0400

    Shorten initialState

--- sudoku.cpp
+++ sudoku.cpp
@@ -13,54 +13,26 @@ class sudoku {
         int arcC[9][9];
 
     sudoku() {
-
+        initialState();
     }
 
     void initialState(){
         int count=0, val=0;
-        ifstream file;
-        string fileName;
-        cout << "Please input the puzzle text file name: " << endl;
-        //open the file and read the data
-        cin >> fileName;
-        file.open(fileName.c_str()); //c_str converts string to char array
+        ifstream file("testing.txt");
         if (file.is_open()) { //this is to check if the file is open
             for (int i=0; i<9; i++) {
                 for (int j=0; j<9; j++) {
-                    file >> val;
-                    variables[i][j] = val;
+                    file >> variables[i][j];
+                    row[i][j] = variables[i][j];
                 }
-            }
-        }
-        //put the row values into the row array
-        //store the enumeration of the row, with the whole row as a list
-        int rowList[9];
-        for (int i=0; i<9; i++) {
-            for (int j=0; j<9; j++) {
-                rowList[j] = variables[i][j];
-            }
-            for (int j=0; j<9; j++) {
-                row[i][j] = rowList[j];
             }
         }
         
         for (int i=0; i<9; i++) {
-            int colList[9];
             for (int j=0; j<9; j++) {
-                colList[j] = variables[j][i];
-            }
-            for (int j=0; j<9; j++) {
-                column[i][j] = colList[j];
+                column[i][j] = variables[j][i];
             }
         }
-        //print column 3
-        for (int i=0; i<9; i++) {
-            cout << column[2][i] << " ";
-        }
-
-
-
-        file.close();
         return;
     }
     
@@ -69,7 +41,8 @@ class sudoku {
 
 int main(int argc, char const *argv[]) {
     sudoku s;
-    s.initialState();
-
+    for (int i=0; i<9; i++){
+        cout << s.row[2][i] << " ";
+    }
     return 0;
 }
