55263d413d6fb1b054b5249da8da91e4a10068f0
meeroTheo <meerbalochandhammy12@gmail.com>
2022-11-03 18:24:13 -0400

    Add: put values from variables into columns/rows

--- sudoku.cpp
+++ sudoku.cpp
@@ -21,30 +21,55 @@ class sudoku {
         ifstream file;
         string fileName;
         cout << "Please input the puzzle text file name: " << endl;
+        //open the file and read the data
         cin >> fileName;
-        file.open(fileName);
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                file >> val;
-                variables[i][j] = val;
-                if (val != 0){
-                    row[i][count]=val;
-                    column[j][count++]=val;
+        file.open(fileName.c_str()); //c_str converts string to char array
+        if (file.is_open()) { //this is to check if the file is open
+            for (int i=0; i<9; i++) {
+                for (int j=0; j<9; j++) {
+                    file >> val;
+                    variables[i][j] = val;
                 }
             }
         }
+        //put the row values into the row array
+        //store the enumeration of the row, with the whole row as a list
+        int rowList[9];
+        for (int i=0; i<9; i++) {
+            for (int j=0; j<9; j++) {
+                rowList[j] = variables[i][j];
+            }
+            for (int j=0; j<9; j++) {
+                row[i][j] = rowList[j];
+            }
+        }
+        
+        for (int i=0; i<9; i++) {
+            int colList[9];
+            for (int j=0; j<9; j++) {
+                colList[j] = variables[j][i];
+            }
+            for (int j=0; j<9; j++) {
+                column[i][j] = colList[j];
+            }
+        }
+        //print column 3
+        for (int i=0; i<9; i++) {
+            cout << column[2][i] << " ";
+        }
+
+
+
         file.close();
         return;
     }
+    
 
 };
 
 int main(int argc, char const *argv[]) {
     sudoku s;
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            cout << s.variables[i][j] << endl;
-        }
-    }
+    s.initialState();
+
     return 0;
 }
