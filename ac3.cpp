9fc4b9db9c5ce40bcb96be51970d4a04355078a1
Frost <frostedwolf789@gmail.com>
2022-11-08 16:26:19 -0500

    add: remaining functions to finish

--- ac3.cpp
+++ ac3.cpp
@@ -3,21 +3,24 @@
 
 class ac3 {
     public:
-        unordered_map<string, int> assigned;
-    // make a queue of constraints 
+        unordered_map<string, int> assignments;
     ac3(sudoku& s){
         setAssigned(s);
     }
 
-    void updateNeighbours(){
-        /* Update row, col., box for the updated var. 
+    bool solve(){
+        /*
         */
+       
+       // make a queue of constraints 
+
+        return true;
     }
 
-    void updateDomains(){
-        /* Update domain for every var. in the same row, col., box
-        */
+    bool revise(){
+        //use in solve
     }
+
     bool backtrack(sudoku s){
         /* Backtrack algorithm
         Look at the variables and their domains
@@ -43,13 +46,13 @@ class ac3 {
         //look for the value with the minimum domain  
         for (int i=0;i<9;i++){
             for (int j=0;j<9;j++){
-                if (assigned.find(to_string(i)+to_string(j))==assigned.end()) //checks if not in the list
+                if (assignments.find(to_string(i)+to_string(j))==assignments.end()) //checks if not in the list
                     pickedVariable.push_back(to_string(i)+to_string(j)); //push the(coordinates?) variable onto the list
                 //if the variable in sudoku is not already assigned
                 //add it to the unassigned list,
                 //next we need to find the minimum variable, which is what we will return 
             }
-        };
+        }
         //sort the pickedVariable list now
         string minVarCoord = pickedVariable[0];
         for (int i=1;i<pickedVariable.size();i++){
@@ -61,13 +64,40 @@ class ac3 {
     }    
 
     void setAssigned(sudoku s){
+        /*
+        Fills the hashmap assigned with the current variables that have a domain of 1 value,
+        therefore, variables that are already set.
+        hashmap contains the coordinate of the assigned value as a key (string) and the value itself as an int
+        */
         for (int i=0;i<9;++i){
             for (int j=0;j<9;++j){
-                if (s.domain[to_string(i)+to_string(j)].size() == 1) 
-                    assigned[to_string(i)+to_string(j)]=s.domain[to_string(i)+to_string(j)][0];
+                if (s.domain[to_string(i)+to_string(j)].size() == 1)    //if domain == 1
+                    assignments[to_string(i)+to_string(j)]=s.domain[to_string(i)+to_string(j)][0]; //add element in domain to assigned
                 
             }
         }
         return;
     }
+
+    int countConstraints(){
+
+    }
+
+    bool consistent(){
+    }
+
+    void addAssigned(){
+
+    }
+
+    void removeAssigned(){
+
+    }
+
+    bool isSolved(){
+
+
+    }
+
+
 };
