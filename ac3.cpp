3a5b823864571574786b6e5f1076dfa7f02731c9
Frost <frostedwolf789@gmail.com>
2022-11-07 20:17:41 -0500

    Add neighbours & assigned

--- ac3.cpp
+++ ac3.cpp
@@ -1,12 +1,12 @@
-
 #include <sudoku.cpp>
 #include <vector>
 
 class ac3 {
     public:
+        unordered_map<string, int> assigned;
     // make a queue of constraints 
-    ac3(){
-
+    ac3(sudoku s){
+        assigned(s);
     }
 
     void updateNeighbours(){
@@ -17,5 +17,18 @@ class ac3 {
     void updateDomains(){
         /* Update domain for every var. in the same row, col., box
         */
+    }
+
+
+
+    void assigned(sudoku s){
+        for (int i=0;i<9;++i){
+            for (int j=0;j<9;++j){
+                if (s.domain[to_string(i)+to_string(j)].size() == 1) 
+                    assigned[to_string(i)+to_string(j)]=s.domain[to_string(i)+to_string(j)][0];
+                
+            }
+        }
+        return;
     }
 };
